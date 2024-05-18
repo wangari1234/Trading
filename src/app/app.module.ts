595b5b7d5d70ea3c4b41ccf06e5097397861e886
Administrator Desktop <Administrator@DESKTOP-854KJI4>
2024-05-18 10:45:00 +0300

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list'; 
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StockGraphsComponent } from './stock-graphs/stock-graphs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    StockGraphsComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient(withFetch()) // Optional configuration
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
